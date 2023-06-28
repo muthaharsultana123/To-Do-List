83e5ab75a7558a053d8519d0bc53b4b57855ddba
muthaharsultana123 <21A95A0513@aec.edu.in>
2023-06-29 01:42:27 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let input =  document.querySelector('#new_task_input')
+let button = document.querySelector('#new_task_submit')
+
+const display = (work) => {
+    let task = document.createElement('div')
+    let text = document.createElement('div')
+    let btn =  document.createElement('button')
+    let _text = document.createTextNode(work)
+    let _btntext = document.createTextNode('DELETE')
+    text.appendChild(_text)
+    btn.appendChild(_btntext)
+    task.appendChild(text)
+    task.appendChild(btn)
+    btn.addEventListener('click',() =>{
+        task.remove()
+    })
+    return task
+}
+
+button.addEventListener('click' , () => {
+    const list = document.querySelector('#list')
+    // list.appendChild(display(input.value))
+    list.innerHTML= display(input.value)
+})
